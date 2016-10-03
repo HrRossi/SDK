91f2c9137837399185517ca768487398d8dca61a
Rodrigo Gutierrez <rodrigo.gutierrez@fove-inc.com>
2016-10-03 10:27:12 +0900

    v0.7.2

--- msvc14/win32/bin/InstallService.bat
+++
@@ -1,3 +0,0 @@
-sc create "FoveRuntime" binPath="%~dp0FoveServiceHost.exe"
-sc sdset FoveRuntime D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)(A;;RPWP;;;WD)
-pause
